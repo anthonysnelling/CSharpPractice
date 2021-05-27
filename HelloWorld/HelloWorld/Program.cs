3571262104d41ac84421f3fa7cae048b30fb056e
Anthony Snelling <anthony.snelling@live.com>
2021-05-26 21:02:16 -0400

    added section that takes basic input, and returns basic output

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace HelloWorld
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+Console.WriteLine("Hello World!");
+Console.WriteLine("A long Journey lies Ahead.");
+Console.WriteLine("A first step starts off every journey.");
+Console.WriteLine("If you keep going you will get far.");
+Console.WriteLine("So what are you waiting for? Lets GO!");
+
+Console.WriteLine("\nBread is ready");
+Console.WriteLine("Who is the bread for?");
+string name = Console.ReadLine();
+Console.WriteLine($"Noted: {name} received bread.");
+
+
+/* Comments in C# */
+Console.WriteLine("\nWhat kind of thing are we talking about");
+// describe the object we are talking about
+string a = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered");
+// store a description for the give object
+string b = Console.ReadLine();
+// Two additional descriptions for this object
+string c = "Doom";
+string d = "3000";
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");

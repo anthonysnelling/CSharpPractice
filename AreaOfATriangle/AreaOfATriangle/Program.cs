e0c4debfd4fd02aff8df1d6ace8013eaf6c722bd
Anthony Snelling <anthony.snelling@live.com>
2021-08-16 19:37:44 -0400

    added a program and solution that finds the area of a triangle

---
+++ AreaOfATriangle/AreaOfATriangle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AreaOfATriangle
+{
+    class Program
+    {
+        private static float area, width, height;
+        private const int DivByTwo = 2;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program finds the area of a triangle!");
+            Console.WriteLine("What is the width of the base of the triangle?");
+            width = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("What is the height of the triangle?");
+           height  = Int32.Parse(Console.ReadLine());
+           area = (height * width) / DivByTwo;
+           Console.WriteLine($"The area of the triangle is: {area}");
+        }
+    }
+}

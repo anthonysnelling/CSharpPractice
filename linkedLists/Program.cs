44e5219112a8b75192add2e1a22a8a13f0c6c8d1
Anthony Snelling <anthony.snelling@live.com>
2021-10-13 18:24:09 -0400

    Working with linked Lists

---
+++ linkedLists/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace linkedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<String> my_list = new LinkedList<string>();
+            
+            my_list.AddLast("Chicken");
+            my_list.AddLast("BlueJay");
+            my_list.AddLast("Cardinal");
+            
+            foreach (string str in my_list)
+            {
+               Console.WriteLine(str); 
+            }
+        }
+        
+    }
+}

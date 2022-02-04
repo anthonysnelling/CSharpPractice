79e36c7caae70d5b5c22757ede635d1358628ff5
Anthony Snelling <anthony.snelling@live.com>
2022-02-04 01:51:54 -0500

    added a section on tuples and a practice program combining tuples and enums

---
+++ ObjectOrientedPrinciples/EnumAndTupleSoup/EnumAndTupleSoup/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Linq;
+
+namespace EnumAndTupleSoup
+{
+    class Program
+    {
+        enum FoodType
+        {
+            Soup,
+            Stew,
+            Gumbo
+        }
+
+        enum MainIngredient
+        {
+            Mushrooms,
+            Chickens,
+            Carrots,
+            Potatoes
+        }
+
+        enum Seasoning
+        {
+            Spicy,
+            Salty,
+            Sweet
+        }
+
+        private static FoodType GetFoodType(string input)
+        {
+            switch (input)
+            {
+                case "soup":
+                    return FoodType.Soup;
+                case "stew":
+                    return FoodType.Stew;
+                case "gumbo":
+                    return FoodType.Gumbo;
+                default:
+                    return FoodType.Gumbo;
+            }
+        }
+
+        private static MainIngredient GetMainIngredient(string input)
+        {
+            switch (input)
+            {
+                case "mushrooms":
+                    return MainIngredient.Mushrooms;
+                case "chickens":
+                    return MainIngredient.Chickens;
+                case "carrots":
+                    return MainIngredient.Carrots;
+                case "potatoes":
+                    return MainIngredient.Potatoes;
+                default:
+                    return MainIngredient.Mushrooms;
+            }
+        }
+
+        private static Seasoning GetSeasoning(string input)
+        {
+            switch (input)
+            {
+                case "salty":
+                    return  Seasoning.Salty;
+                case "sweet":
+                    return Seasoning.Sweet;
+                case "spicy":
+                    return Seasoning.Spicy;
+                default:
+                    return Seasoning.Salty;
+            }
+        }
+
+        private static (FoodType, MainIngredient, Seasoning) MakeASoup()
+        {
+            (FoodType, MainIngredient, Seasoning) soup;
+            Console.WriteLine("Lets start with the type: soup, stew, or gumbo?");
+            FoodType soupType;
+
+            string input = "";
+            bool validationCheck = false;
+            while (!validationCheck)
+            {
+                input = Console.ReadLine();
+                validationCheck = StringValidation(input);
+            }
+
+            soupType = GetFoodType(input);
+
+
+            Console.WriteLine("Alright choose a Main ingredient: mushrooms, chickens, carrots, or potatoes");
+            MainIngredient mainIngredient;
+
+            input = "";
+            validationCheck = false;
+            while (!validationCheck)
+            {
+                input = Console.ReadLine();
+                validationCheck = StringValidation(input);
+            }
+            mainIngredient = GetMainIngredient(input);
+
+
+            Console.WriteLine("Now Choose the Seasoning: spicy, salty, sweet");
+            Seasoning seasoning;
+
+            input = "";
+            validationCheck = false;
+            while (!validationCheck)
+            {
+                input = Console.ReadLine();
+                validationCheck = StringValidation(input);
+            }
+            seasoning = GetSeasoning(input);
+
+            soup = (soupType, mainIngredient, seasoning);
+            return soup;
+        }
+
+        private static bool StringValidation(string input)
+        {
+            switch (input)
+            {
+                case "spicy":
+                    return true;
+                case "salty":
+                    return true;
+                case "sweet":
+                    return true;
+                case "mushrooms":
+                    return true;
+                case "chickens":
+                    return true;
+                case "carrots":
+                    return true;
+                case "potatoes":
+                    return true;
+                case "soup":
+                    return true;
+                case "stew":
+                    return true;
+                case "gumbo":
+                    return true;
+                default:
+                    Console.WriteLine("Incorrect type, please try again.");
+                    return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var foodWeHaveMade = new (FoodType typeOfFood, MainIngredient mainIngredient, Seasoning seasoning) [3];
+            Console.WriteLine("Greetings we will make threes soups!");
+
+            Console.WriteLine("Here is the first pot!");
+            foodWeHaveMade[0] = (MakeASoup());
+
+            Console.WriteLine("\nTime for pot two!");
+            foodWeHaveMade[1] = (MakeASoup());
+
+            Console.WriteLine("\nNow for the final pot!");
+            foodWeHaveMade[2] = (MakeASoup());
+
+            Console.WriteLine("Here are the soups we have made!");
+            Console.WriteLine(
+                $"First soup: {foodWeHaveMade[0].seasoning} {foodWeHaveMade[0].mainIngredient} {foodWeHaveMade[0].typeOfFood}");
+            Console.WriteLine(
+                $"Second soup: {foodWeHaveMade[1].seasoning} {foodWeHaveMade[1].mainIngredient} {foodWeHaveMade[1].typeOfFood}");
+            Console.WriteLine(
+                $"Third soup: {foodWeHaveMade[2].seasoning} {foodWeHaveMade[2].mainIngredient} {foodWeHaveMade[2].typeOfFood}");
+        }
+    }
+}
---
+++ ObjectOrientedPrinciples/Tuples/Tuples/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Common;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
+namespace Tuples
+{
+    class Program
+    {
+        enum Tiletype
+        {
+            Grass,
+            Water,
+            Rock
+        }
+
+        static void Main(string[] args)
+        {
+            // tuples are a composite type of multiple elements
+            // you can form tuples with the use of parentheses
+            (string, int, int) score = ("Tony", 90000, 1);
+
+            // you can use var to help out with the typing
+            var score2 = ("Abby", 140000, 2);
+
+            // you can access the items individually like so
+            Console.WriteLine($"Name:{score.Item1}, Level:{score.Item3}, Score:{score.Item2}");
+            Console.WriteLine($"Name:{score2.Item1}, Level:{score2.Item3}, Score:{score2.Item2}");
+
+            //If you don't use var you can name the elements things other than item1, item2, etc.
+            (string name, int model, string color) car = ("Pontiac G6", 2009, "gold");
+            Console.WriteLine($"\nI own a {car.color} {car.name} from {car.model}");
+            // there is a way to do it with var, but the types are inferred and the syntax is different
+            var car2 = (name: "Chevy Cavalier", model: 2005, color: "silver");
+            Console.WriteLine($"My last car was a {car2.color} {car2.model} {car2.name}");
+
+            //vars can be useful for coding quick and concise, but you can't use vars as parameters
+            // be careful if you need to pass a tuple as a parameter or return it in a funct ion
+
+            // you can even use a tuple and an enum together
+            var tile = (Row: 2, Column: 4, Type: Tiletype.Grass);
+            
+            // You can make a tuple array
+            (string name, int points, int level)[] CreateHighScores()
+            {
+                return new (string, int, int)[3]
+                {
+                    ("Tom", 12420, 15),
+                    ("Alan", 8453, 9),
+                    ("Carol", 900, 3),
+                };
+            }
+        }
+    }
+}
